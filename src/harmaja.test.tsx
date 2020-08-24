c6635089f2ed8b6267b337a31c1ab743c6bcae30
Juha Paananen <juha.paananen@gmail.com>
2020-08-24 18:57:47 +0300

    more tests

--- src/harmaja.test.tsx
+++ src/harmaja.test.tsx
@@ -2,6 +2,7 @@ import { h } from "./index"
 import * as H from "./index"
 import * as B from "baconjs"
 import { DOMElement, removeElement } from "./harmaja"
+import { testRender, htmlOf } from "./test-utils"
 
 describe("Harmaja", () => {
     it("Creating elements without JSX", () => {
@@ -56,21 +57,4 @@ describe("Harmaja", () => {
         expect(() => (<h1>{new Date()}</h1>)).toThrow()
         expect(() => (<h1>{({})}</h1>)).toThrow()
     })
-
-    function htmlOf(element: H.DOMElement) {
-        if (element instanceof HTMLElement) {
-            return element.outerHTML;
-        } else {
-            return element.textContent
-        }
-    }
 })
-
-function testRender<T>(init: T, test: (property: B.Property<T>, set: (v: T) => any) => DOMElement) {
-    const bus = new B.Bus<T>()
-    const property = bus.toProperty(init)
-    const element = test(property, bus.push)
-    removeElement(element)
-    // Verify that all subscribers are removed on unmount
-    expect((property as any).dispatcher.subscriptions.length).toEqual(0)
-}
