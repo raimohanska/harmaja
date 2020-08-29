a139b828d408ccd53d275b9f89d54c739ad9f5d1
Juha Paananen <juha.paananen@gmail.com>
2020-08-29 10:40:22 +0300

    dom-hello-world

---
+++ examples/first/index.tsx
@@ -0,0 +1,13 @@
+import * as H from "../../src/index"
+// All the DOM API you're gonna need!
+
+// Creating elements and text nodes
+const element = document.createElement("h1")
+element.appendChild(document.createTextNode("HELLO WORLD"))
+document.getElementById("root")!.appendChild(element)
+
+// Event handlers
+element.onclick = (e: MouseEvent) => alert("HELLO")
+
+// Setting style as string
+element.setAttribute("style", "text-decoration: underline;");
