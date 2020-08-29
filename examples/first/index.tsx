04900eb3d0c85c16583e47b9bfd2539145ac7269
Juha Paananen <juha.paananen@gmail.com>
2020-08-29 10:55:44 +0300

    simple createElement

--- examples/first/index.tsx
+++ examples/first/index.tsx
@@ -1,13 +1,21 @@
-import * as H from "../../src/index"
+import * as React from "../../src/index"
 // All the DOM API you're gonna need!
 
 // Creating elements and text nodes
 const element = document.createElement("h1")
 element.appendChild(document.createTextNode("HELLO WORLD"))
-document.getElementById("root")!.appendChild(element)
 
 // Event handlers
 element.onclick = (e: MouseEvent) => alert("HELLO")
 
 // Setting style as string
 element.setAttribute("style", "text-decoration: underline;");
+
+document.getElementById("root")!.appendChild(<div>
+        <h1>Hello world</h1>
+        <ul>
+            <li>Reaktor</li>
+            <li>Is</li>
+            <li>Best</li>
+        </ul>
+    </div>)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 import B from "baconjs"
+
+export function createElement(type: string, props: Record<string, any>, ...children: any[]) {
+    const element = document.createElement(type)
+    for (const [key, value] of Object.entries(props || {})) {
+        (element as any)[key] = value
+    }
+    for (const child of children) {
+        if (typeof child === "string") {
+            element.appendChild(document.createTextNode(child))
+        } else {            
+            element.appendChild(child)
+        }
+    }
+    return element
+}
