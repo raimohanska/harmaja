0a1f558312f117ab11814b5e68b3078103071760
Juha Paananen <juha.paananen@gmail.com>
2020-08-30 11:10:31 +0300

    remove the "capture" variants of events from types

--- src/jsxfactory.ts
+++ src/jsxfactory.ts
@@ -472,31 +472,20 @@ declare global {
 
     interface DOMAttributes<T> {
         children?: ChildrenType;
-        dangerouslySetInnerHTML?: {
-            __html: string;
-        };
 
         // Clipboard Events
         onCopy?: ClipboardEventHandler<T>;
-        onCopyCapture?: ClipboardEventHandler<T>;
         onCut?: ClipboardEventHandler<T>;
-        onCutCapture?: ClipboardEventHandler<T>;
         onPaste?: ClipboardEventHandler<T>;
-        onPasteCapture?: ClipboardEventHandler<T>;
 
         // Composition Events
         onCompositionEnd?: CompositionEventHandler<T>;
-        onCompositionEndCapture?: CompositionEventHandler<T>;
         onCompositionStart?: CompositionEventHandler<T>;
-        onCompositionStartCapture?: CompositionEventHandler<T>;
         onCompositionUpdate?: CompositionEventHandler<T>;
-        onCompositionUpdateCapture?: CompositionEventHandler<T>;
 
         // Focus Events
         onFocus?: FocusEventHandler<T>;
-        onFocusCapture?: FocusEventHandler<T>;
         onBlur?: FocusEventHandler<T>;
-        onBlurCapture?: FocusEventHandler<T>;
 
         // Form Events
         onChange?: EventHandler<ChangeEvent<T>>;
@@ -507,157 +496,90 @@ declare global {
 
         // Image Events
         onLoad?: EventHandler<HarmajaEvent<T>>;
-        onLoadCapture?: EventHandler<HarmajaEvent<T>>;
         onError?: EventHandler<HarmajaEvent<T>>; // also a Media Event
-        onErrorCapture?: EventHandler<HarmajaEvent<T>>; // also a Media Event
 
         // Keyboard Events
         onKeyDown?: KeyboardEventHandler<T>;
-        onKeyDownCapture?: KeyboardEventHandler<T>;
         onKeyPress?: KeyboardEventHandler<T>;
-        onKeyPressCapture?: KeyboardEventHandler<T>;
         onKeyUp?: KeyboardEventHandler<T>;
-        onKeyUpCapture?: KeyboardEventHandler<T>;
 
         // Media Events
         onAbort?: EventHandler<HarmajaEvent<T>>;
-        onAbortCapture?: EventHandler<HarmajaEvent<T>>;
         onCanPlay?: EventHandler<HarmajaEvent<T>>;
-        onCanPlayCapture?: EventHandler<HarmajaEvent<T>>;
         onCanPlayThrough?: EventHandler<HarmajaEvent<T>>;
-        onCanPlayThroughCapture?: EventHandler<HarmajaEvent<T>>;
         onDurationChange?: EventHandler<HarmajaEvent<T>>;
-        onDurationChangeCapture?: EventHandler<HarmajaEvent<T>>;
         onEmptied?: EventHandler<HarmajaEvent<T>>;
-        onEmptiedCapture?: EventHandler<HarmajaEvent<T>>;
         onEncrypted?: EventHandler<HarmajaEvent<T>>;
-        onEncryptedCapture?: EventHandler<HarmajaEvent<T>>;
         onEnded?: EventHandler<HarmajaEvent<T>>;
-        onEndedCapture?: EventHandler<HarmajaEvent<T>>;
         onLoadedData?: EventHandler<HarmajaEvent<T>>;
-        onLoadedDataCapture?: EventHandler<HarmajaEvent<T>>;
         onLoadedMetadata?: EventHandler<HarmajaEvent<T>>;
-        onLoadedMetadataCapture?: EventHandler<HarmajaEvent<T>>;
         onLoadStart?: EventHandler<HarmajaEvent<T>>;
-        onLoadStartCapture?: EventHandler<HarmajaEvent<T>>;
         onPause?: EventHandler<HarmajaEvent<T>>;
-        onPauseCapture?: EventHandler<HarmajaEvent<T>>;
         onPlay?: EventHandler<HarmajaEvent<T>>;
-        onPlayCapture?: EventHandler<HarmajaEvent<T>>;
         onPlaying?: EventHandler<HarmajaEvent<T>>;
-        onPlayingCapture?: EventHandler<HarmajaEvent<T>>;
         onProgress?: EventHandler<HarmajaEvent<T>>;
-        onProgressCapture?: EventHandler<HarmajaEvent<T>>;
         onRateChange?: EventHandler<HarmajaEvent<T>>;
-        onRateChangeCapture?: EventHandler<HarmajaEvent<T>>;
         onSeeked?: EventHandler<HarmajaEvent<T>>;
-        onSeekedCapture?: EventHandler<HarmajaEvent<T>>;
         onSeeking?: EventHandler<HarmajaEvent<T>>;
-        onSeekingCapture?: EventHandler<HarmajaEvent<T>>;
         onStalled?: EventHandler<HarmajaEvent<T>>;
-        onStalledCapture?: EventHandler<HarmajaEvent<T>>;
         onSuspend?: EventHandler<HarmajaEvent<T>>;
-        onSuspendCapture?: EventHandler<HarmajaEvent<T>>;
         onTimeUpdate?: EventHandler<HarmajaEvent<T>>;
-        onTimeUpdateCapture?: EventHandler<HarmajaEvent<T>>;
         onVolumeChange?: EventHandler<HarmajaEvent<T>>;
-        onVolumeChangeCapture?: EventHandler<HarmajaEvent<T>>;
         onWaiting?: EventHandler<HarmajaEvent<T>>;
-        onWaitingCapture?: EventHandler<HarmajaEvent<T>>;
 
         // MouseEvents
         onAuxClick?: MouseEventHandler<T>;
-        onAuxClickCapture?: MouseEventHandler<T>;
         onClick?: MouseEventHandler<T>;
-        onClickCapture?: MouseEventHandler<T>;
         onContextMenu?: MouseEventHandler<T>;
-        onContextMenuCapture?: MouseEventHandler<T>;
         onDoubleClick?: MouseEventHandler<T>;
-        onDoubleClickCapture?: MouseEventHandler<T>;
         onDrag?: DragEventHandler<T>;
-        onDragCapture?: DragEventHandler<T>;
         onDragEnd?: DragEventHandler<T>;
-        onDragEndCapture?: DragEventHandler<T>;
         onDragEnter?: DragEventHandler<T>;
-        onDragEnterCapture?: DragEventHandler<T>;
         onDragExit?: DragEventHandler<T>;
-        onDragExitCapture?: DragEventHandler<T>;
         onDragLeave?: DragEventHandler<T>;
-        onDragLeaveCapture?: DragEventHandler<T>;
         onDragOver?: DragEventHandler<T>;
-        onDragOverCapture?: DragEventHandler<T>;
         onDragStart?: DragEventHandler<T>;
-        onDragStartCapture?: DragEventHandler<T>;
         onDrop?: DragEventHandler<T>;
-        onDropCapture?: DragEventHandler<T>;
         onMouseDown?: MouseEventHandler<T>;
-        onMouseDownCapture?: MouseEventHandler<T>;
         onMouseEnter?: MouseEventHandler<T>;
         onMouseLeave?: MouseEventHandler<T>;
         onMouseMove?: MouseEventHandler<T>;
-        onMouseMoveCapture?: MouseEventHandler<T>;
         onMouseOut?: MouseEventHandler<T>;
-        onMouseOutCapture?: MouseEventHandler<T>;
         onMouseOver?: MouseEventHandler<T>;
-        onMouseOverCapture?: MouseEventHandler<T>;
         onMouseUp?: MouseEventHandler<T>;
-        onMouseUpCapture?: MouseEventHandler<T>;
 
         // Selection Events
         onSelect?: EventHandler<HarmajaEvent<T>>;
-        onSelectCapture?: EventHandler<HarmajaEvent<T>>;
 
         // Touch Events
         onTouchCancel?: TouchEventHandler<T>;
-        onTouchCancelCapture?: TouchEventHandler<T>;
         onTouchEnd?: TouchEventHandler<T>;
-        onTouchEndCapture?: TouchEventHandler<T>;
         onTouchMove?: TouchEventHandler<T>;
-        onTouchMoveCapture?: TouchEventHandler<T>;
         onTouchStart?: TouchEventHandler<T>;
-        onTouchStartCapture?: TouchEventHandler<T>;
 
         // Pointer Events
         onPointerDown?: PointerEventHandler<T>;
-        onPointerDownCapture?: PointerEventHandler<T>;
         onPointerMove?: PointerEventHandler<T>;
-        onPointerMoveCapture?: PointerEventHandler<T>;
         onPointerUp?: PointerEventHandler<T>;
-        onPointerUpCapture?: PointerEventHandler<T>;
         onPointerCancel?: PointerEventHandler<T>;
-        onPointerCancelCapture?: PointerEventHandler<T>;
         onPointerEnter?: PointerEventHandler<T>;
-        onPointerEnterCapture?: PointerEventHandler<T>;
         onPointerLeave?: PointerEventHandler<T>;
-        onPointerLeaveCapture?: PointerEventHandler<T>;
         onPointerOver?: PointerEventHandler<T>;
-        onPointerOverCapture?: PointerEventHandler<T>;
         onPointerOut?: PointerEventHandler<T>;
-        onPointerOutCapture?: PointerEventHandler<T>;
-        onGotPointerCapture?: PointerEventHandler<T>;
-        onGotPointerCaptureCapture?: PointerEventHandler<T>;
-        onLostPointerCapture?: PointerEventHandler<T>;
-        onLostPointerCaptureCapture?: PointerEventHandler<T>;
 
         // UI Events
         onScroll?: UIEventHandler<T>;
-        onScrollCapture?: UIEventHandler<T>;
 
         // Wheel Events
         onWheel?: WheelEventHandler<T>;
-        onWheelCapture?: WheelEventHandler<T>;
 
         // Animation Events
         onAnimationStart?: AnimationEventHandler<T>;
-        onAnimationStartCapture?: AnimationEventHandler<T>;
         onAnimationEnd?: AnimationEventHandler<T>;
-        onAnimationEndCapture?: AnimationEventHandler<T>;
         onAnimationIteration?: AnimationEventHandler<T>;
-        onAnimationIterationCapture?: AnimationEventHandler<T>;
 
         // Transition Events
         onTransitionEnd?: TransitionEventHandler<T>;
-        onTransitionEndCapture?: TransitionEventHandler<T>;
     }
 
     export interface CSSProperties extends CSS.Properties<string | number> {
