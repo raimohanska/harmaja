c143c1515c4134cfab20995789c612df7eb22002
Juha Paananen <juha.paananen@gmail.com>
2020-10-04 09:50:55 +0300

    a bit more aggressive and hacky way to ensure we get the current value of a Property synchronously

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,23 +4,19 @@ export const valueMissing = {}
 export type ValueMissing = typeof valueMissing
 
 export function getCurrentValue<A>(observable: B.Property<A>): A {
-    let currentV: any = valueMissing;
-    if ((observable as any).get) {
-      currentV = (observable as any).get(); // For Atoms
-    } else {   
-      const unsub = observable.subscribeInternal(e => {
-          if (B.hasValue(e)) {
-              currentV = e.value;
-          }
-      })       
-      unsub();
-    }
-    if (currentV === valueMissing) {
-        console.log("Current value not found!", observable);
-        throw new Error("Current value missing. Cannot render. " + observable);
-    }      
-    return currentV;
-  };
+  let currentV: any = valueMissing;
+  if ((observable as any).get) {
+    currentV = (observable as any).get(); // For Atoms
+  } else {   
+    observable.subscribe()() // force temporary subscription
+    return (observable as any).dispatcher.current.get().value
+  }
+  if (currentV === valueMissing) {
+      console.log("Current value not found!", observable);
+      throw new Error("Current value missing. Cannot render. " + observable);
+  }      
+  return currentV;
+};
 
 export function reportValueMissing(observable: B.Observable<any>): never {
     console.log("Current value not found!", observable);
