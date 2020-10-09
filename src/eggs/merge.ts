28fd1f401a78938c5bb5dd97d677207fe38d40d6
Juha Paananen <juha.paananen@gmail.com>
2020-10-09 17:24:36 +0300

    laxer types for merge

--- src/eggs/merge.ts
+++ src/eggs/merge.ts
@@ -2,7 +2,9 @@ import { EventStream, EventStreamSeed } from "./abstractions";
 import { StatelessEventStream } from "./eventstream";
 
 export function merge<A>(a: EventStream<A>, b: EventStream<A>): EventStream<A>;
+export function merge<A, B>(a: EventStream<A>, b: EventStream<B>): EventStream<A | B>;
 export function merge<A>(a: EventStreamSeed<A>, b: EventStreamSeed<A>): EventStreamSeed<A>;
+export function merge<A, B>(a: EventStreamSeed<A>, b: EventStreamSeed<B>): EventStreamSeed<A | B>;
 export function merge<A>(...streams: (EventStream<any> | EventStreamSeed<any>)[]) {
     const seed = new EventStreamSeed<A>(`merge(${streams})`, observer => {
         const unsubs = streams.map(s => s.forEach(observer))
